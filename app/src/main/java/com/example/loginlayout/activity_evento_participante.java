5d5f71c4c93d7b654d2d287719e405b44af8a417
agustic <agustic@DESKTOP-RPMV31T>
2019-12-24 11:00:00 +0100

    Apuntarse/Desapuntarse acabat

---
+++ app/src/main/java/com/example/loginlayout/activity_evento_participante.java
@@ -0,0 +1,162 @@
+package com.example.loginlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.text.NumberFormat;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class activity_evento_participante extends AppCompatActivity implements Interfaz {
+
+    private TextView deporteText;
+    private TextView fechaText;
+    private TextView horaText;
+    private TextView numeroParticipantes;
+    private TextView numeroAsistentes;
+    private TextView descripcionText;
+    private TextView eventoText;
+    private Button buttonApuntarse;
+    private String idEvento;
+    private String idUser;
+    private String assistir;
+    private boolean resultado;
+    private Context context = this;
+    private RequestQueue queue; //cola de las solicitudes
+    private int llamada;
+    private View view;
+
+    boolean estaApuntado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento_participante);
+
+        deporteText = findViewById(R.id.deporte);
+        fechaText = findViewById(R.id.tv_fecha);
+        horaText = findViewById(R.id.tv_hora2);
+        numeroParticipantes = findViewById(R.id.numeroParticipantes);
+        numeroAsistentes = findViewById(R.id.numeroAsistentes);
+        descripcionText = findViewById(R.id.descriptionText);
+        eventoText = findViewById(R.id.eventoText);
+        buttonApuntarse = findViewById(R.id.buttonApuntarse);
+
+        idEvento = "5ddeb5c0b43de93e240d015d";
+        idUser = "5de2a62cb43de93e240d0166";
+
+        llamada = 1;
+        Connection con = new Connection(this);
+        con.execute("http://10.4.41.144:3000/event/info/"+idEvento,"GET", null);
+    }
+
+
+    public void ApuntarseDesapuntarse(View v){
+        llamada=2;
+        Connection con = new Connection(this);
+        con.execute("http://10.4.41.144:3000/event/includes/" + idEvento +"/" + idUser,"GET", null);
+
+        System.out.println("ENTRA A LA FUNCION");
+
+        if(estaApuntado){
+            llamada=3;
+            con = new Connection(this);
+            con.execute("http://10.4.41.144:3000/event/unassign/" + idUser +"/" + idEvento,"POST", null);
+            System.out.println("PETA A LA FUNCION APUNTARSE");
+        }
+        else{
+            llamada=4;
+            con = new Connection(this);
+            con.execute("http://10.4.41.144:3000/event/assign/" + idUser +"/" + idEvento,"POST", null);
+            System.out.println("PETA A LA FUNCION DESAPUNTARSE");
+        }
+        System.out.println("SALE FUNCION");
+    }
+
+    @Override
+    public void Respuesta(JSONObject datos) {
+        if(llamada == 1) {//Obtener información del evento
+            try {
+                deporteText.setText("Evento de " + datos.getString("sport"));
+                descripcionText.setText(datos.getString("description"));
+                horaText.setText(datos.getString("date").substring(11, 16));
+                fechaText.setText(datos.getString("date").substring(0, 10));
+                numeroParticipantes.setText(Integer.toString(datos.getInt("max_users")));
+                numeroAsistentes.setText(Integer.toString(datos.getInt("initial_users")));
+                eventoText.setText(datos.getString("level"));
+
+                llamada = 2;
+                Connection con = new Connection(this);
+                con.execute("http://10.4.41.144:3000/event/includes/" + idEvento +"/" + idUser,"GET", null);
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(llamada == 2){ //Mirar si esta apuntado
+            try {
+                estaApuntado = datos.getBoolean("response");
+                if(estaApuntado){
+                    buttonApuntarse.setText("DESAPUNTARSE");
+                }
+                else buttonApuntarse.setText("APUNTARSE");
+                System.out.println("ESTA APUNTADO?" + estaApuntado);
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(llamada == 3){ //Desapuntarse
+            try {
+                if (datos.getInt("codigo") == 200) {
+                    Intent i = new Intent(this, activity_evento_participante.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(activity_evento_participante.this, "Error", LENGTH_SHORT).show();
+                }
+                final Connection con = new Connection(this);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(llamada == 4){ //Apuntarse
+            try {
+                //System.out.println("ERROR: " + datos.getInt("codigo"));
+                if (datos.getInt("codigo") == 200) {
+                    Intent i = new Intent(this, activity_evento_participante.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(activity_evento_participante.this, "Error", LENGTH_SHORT).show();
+                }
+                final Connection con = new Connection(this);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
